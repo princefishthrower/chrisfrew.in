8cdc283dee4f32d233788c14d9a9c23f94be551b
Chris Frewin <frewin.christopher@gmail.com>
2024-02-18 21:29:34 +0100

    add audio to some blog posts!

---
+++ src/components/utils/AudioPlayer.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+export interface IAudioPlayerProps {
+    audioSrc: string
+}
+
+const AudioPlayer = (props: IAudioPlayerProps) => {
+    const { audioSrc } = props
+    return (
+        <div style={{display:"flex", flexDirection:"column", justifyContent: "center", alignItems:"center", fontSize: "0.75rem", margin:"5px"}}>
+            <i>Listen to AI Chris™ read this post aloud:</i>
+            <audio src={audioSrc} preload="auto" controls={true} style={{margin:"5px"}}/>
+            <i>
+                Brought to you by
+                <a href="https://github.com/codevideo/pontificator"><code>pontificator</code></a>, a
+                Full Stack Craft product.
+            </i>
+        </div>
+    )
+}
+
+export default AudioPlayer
